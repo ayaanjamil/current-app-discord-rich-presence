5ccb7db5da2e1f1286f10a31998fa78008ce2d7c
Ayaan Jamil <39400870+ayaanjamil@users.noreply.github.com>
2021-06-15 21:15:14 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+import psutil, win32process, win32gui, time
+from pypresence import Presence
+
+
+def active_window_process_name():
+    pid = win32process.GetWindowThreadProcessId(
+        win32gui.GetForegroundWindow())
+    return psutil.Process(pid[-1]).name()
+
+
+itemlist = ['discord', 'spotify', 'chrome', 'teams', 'zoom']
+client_id = '852940672629080066'
+RPC = Presence(client_id)
+RPC.connect()
+starttime = time.time()
+
+while True:
+    try:
+        x = active_window_process_name()
+        x = x[:-4]
+        print(x)
+        if x.lower() in itemlist:
+            print('works ig')
+            print(RPC.update(state=f"Playing {x.capitalize()}", details="yes", large_image=f"{x.lower()}",
+                             large_text="pls dont crash", start=starttime))
+        time.sleep(10)
+    except Exception:
+        pass
